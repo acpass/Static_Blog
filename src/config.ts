de8a178b1ebd7d03e837fc9d8405f326df2166b1
acpass <lyc867123276@gmail.com>
2024-08-31 17:43:54 +0800

    add an testpost

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
+  title: 'ACPASS Demo Blog',
   subtitle: 'Demo Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
---
+++ src/content/posts/testpost.md
@@ -0,0 +1,10 @@
+---
+title: testpost
+published: 2024-08-31
+description: ''
+image: ''
+tags: []
+category: ''
+draft: false 
+language: ''
+---
